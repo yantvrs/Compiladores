73b842e3861454f16cb9a6737acf3d45b4f4ac3c
yantvrs <yantavares2002@gmail.com>
2024-10-12 09:50:17 -0300

    Remoção arquivos hello

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Exibe a mensagem "Hello, World!" na tela
-    printf("Hello, World!\n");
-    
-    return 0;  // Indica que o programa terminou corretamente
-}
