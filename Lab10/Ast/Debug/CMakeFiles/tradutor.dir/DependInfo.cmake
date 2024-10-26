229f6902bb4c153b5a16ea9b7dfd7fdeee5f6633
yantvrs <yantavares2002@gmail.com>
2024-10-25 21:43:12 -0300

    Lab2

---
+++ Lab10/Ast/Debug/CMakeFiles/tradutor.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab10/Ast/ast.cpp" "CMakeFiles/tradutor.dir/ast.cpp.o" "gcc" "CMakeFiles/tradutor.dir/ast.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab10/Ast/checker.cpp" "CMakeFiles/tradutor.dir/checker.cpp.o" "gcc" "CMakeFiles/tradutor.dir/checker.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab10/Ast/error.cpp" "CMakeFiles/tradutor.dir/error.cpp.o" "gcc" "CMakeFiles/tradutor.dir/error.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab10/Ast/lexer.cpp" "CMakeFiles/tradutor.dir/lexer.cpp.o" "gcc" "CMakeFiles/tradutor.dir/lexer.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab10/Ast/parser.cpp" "CMakeFiles/tradutor.dir/parser.cpp.o" "gcc" "CMakeFiles/tradutor.dir/parser.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab10/Ast/symtable.cpp" "CMakeFiles/tradutor.dir/symtable.cpp.o" "gcc" "CMakeFiles/tradutor.dir/symtable.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab10/Ast/tradutor.cpp" "CMakeFiles/tradutor.dir/tradutor.cpp.o" "gcc" "CMakeFiles/tradutor.dir/tradutor.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
