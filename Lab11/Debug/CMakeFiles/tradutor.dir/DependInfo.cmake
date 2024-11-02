6cb942a18635aa96a55d85463d50edbe2ba19da9
yan_TVRS <yantavares2002@gmail.com>
2024-11-02 12:05:20 -0300

    Laboratório 3 de compiladores

---
+++ Lab11/Debug/CMakeFiles/tradutor.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab11/ast.cpp" "CMakeFiles/tradutor.dir/ast.cpp.o" "gcc" "CMakeFiles/tradutor.dir/ast.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab11/checker.cpp" "CMakeFiles/tradutor.dir/checker.cpp.o" "gcc" "CMakeFiles/tradutor.dir/checker.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab11/error.cpp" "CMakeFiles/tradutor.dir/error.cpp.o" "gcc" "CMakeFiles/tradutor.dir/error.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab11/gen.cpp" "CMakeFiles/tradutor.dir/gen.cpp.o" "gcc" "CMakeFiles/tradutor.dir/gen.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab11/lexer.cpp" "CMakeFiles/tradutor.dir/lexer.cpp.o" "gcc" "CMakeFiles/tradutor.dir/lexer.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab11/parser.cpp" "CMakeFiles/tradutor.dir/parser.cpp.o" "gcc" "CMakeFiles/tradutor.dir/parser.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab11/symtable.cpp" "CMakeFiles/tradutor.dir/symtable.cpp.o" "gcc" "CMakeFiles/tradutor.dir/symtable.cpp.o.d"
+  "/mnt/c/Users/SAMSUNG/Code/TrabalhosCompiladores/Compiladores/Lab11/tradutor.cpp" "CMakeFiles/tradutor.dir/tradutor.cpp.o" "gcc" "CMakeFiles/tradutor.dir/tradutor.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab11/Debug/CMakeFiles/tradutor.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tradutor.dir/ast.cpp.o"
+  "CMakeFiles/tradutor.dir/ast.cpp.o.d"
+  "CMakeFiles/tradutor.dir/checker.cpp.o"
+  "CMakeFiles/tradutor.dir/checker.cpp.o.d"
+  "CMakeFiles/tradutor.dir/error.cpp.o"
+  "CMakeFiles/tradutor.dir/error.cpp.o.d"
+  "CMakeFiles/tradutor.dir/gen.cpp.o"
+  "CMakeFiles/tradutor.dir/gen.cpp.o.d"
+  "CMakeFiles/tradutor.dir/lexer.cpp.o"
+  "CMakeFiles/tradutor.dir/lexer.cpp.o.d"
+  "CMakeFiles/tradutor.dir/parser.cpp.o"
+  "CMakeFiles/tradutor.dir/parser.cpp.o.d"
+  "CMakeFiles/tradutor.dir/symtable.cpp.o"
+  "CMakeFiles/tradutor.dir/symtable.cpp.o.d"
+  "CMakeFiles/tradutor.dir/tradutor.cpp.o"
+  "CMakeFiles/tradutor.dir/tradutor.cpp.o.d"
+  "tradutor"
+  "tradutor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tradutor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
